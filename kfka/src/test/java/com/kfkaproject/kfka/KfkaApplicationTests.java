31031f79fab1439cc49611bca584113245b236e4
itsAllGoose <cryanneill@outlook.com>
2023-06-08 23:22:25 +0100

    Init commit

---
+++ kfka/src/test/java/com/kfkaproject/kfka/KfkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfkaproject.kfka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
