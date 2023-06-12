8577d732aa6cb45c112b9986834e7a9e685c3a9d
itsAllGoose <cryanneill@outlook.com>
2023-06-12 21:22:04 +0100

    Spring JDBC and postgres set up - builds and runs

--- kfka/src/main/java/com/kfkaproject/kfka/KfkaApplication.java
+++ kfka/src/main/java/com/kfkaproject/kfka/KfkaApplication.java
@@ -1,9 +1,11 @@
 package com.kfkaproject.kfka;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class KfkaApplication {
 
 	public static void main(String[] args) {
